4492d49838542fc45dcd8375b6fb3533fbd09ccb
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-09-30 07:27:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere(e) {
             const radius = parseFloat(document.getElementById("radius").value);
             if (!isNaN(radius) && radius >= 0) {
                 const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-                document.getElementById("volume").value = volume.toFixed(2);
+                document.getElementById("volume").value = volume.toFixed(4);
             } else {
                 document.getElementById("volume").value = "Invalid input";
             }

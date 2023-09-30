cf70903e41dd238218d85af11e6e6088e11b2f37
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-09-30 07:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(e) {
+            const radius = parseFloat(document.getElementById("radius").value);
+            if (!isNaN(radius) && radius >= 0) {
+                const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+                document.getElementById("volume").value = volume.toFixed(2);
+            } else {
+                document.getElementById("volume").value = "Invalid input";
+            }
+	   e.preventDefault();
+        }
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
